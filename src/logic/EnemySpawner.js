284db8d8a17d41f77aa8b28a279e295a8426fa38
Dmitrii Gritsai <gritsdmi@fel.cvut.cz>
2020-05-14 21:35:09 +0200

    create enemy spawner

---
+++ src/logic/EnemySpawner.js
@@ -0,0 +1,34 @@
+import Phaser from 'phaser'
+import Enemy from "../objects/Enemy"
+
+export default class EnemySpawner {
+
+	constructor(scene, enemyKey = 'enemy'){
+		this.scene = scene
+		this.key = enemyKey
+		this.spawnDelay = 2000
+		this.startDelay = 1
+		this.startDelay = this.startDelay * (-1)
+
+		// this._group = this.scene.physics.add.group()
+
+		this.scene.time.addEvent({
+			delay: this.spawnDelay,
+			repeat: 50,
+			callback: ()=>{
+			    // spawn a bullet
+			    this.spawn()
+			},
+			startAt : this.startDelay
+			// loop: true
+		})
+	}
+
+	spawn(){
+		// this.group.create()
+
+		this.scene.enemies.add(new Enemy(this.scene, 40,500,this.key))
+
+	}
+
+}
